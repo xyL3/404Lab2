e65317801d7bf14cfecb00890683e4d324742369
Xueying Luo <snowvan113@gmail.com>
2021-09-17 11:16:32 -0600

    add client and server files

---
+++ proxy_client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from echo_server import BUFFER_SIZE
+import socket
+
+#define address, buffer size 
+HOST = "localhost"
+PORT = 8001
+BUFFER_SIZE = 1024
+
+payload = "GET / HTTP/1.0\r\nHOST: www.google.com\r\n\r\n"
+
+def connect(addr):
+    #create socket, connect, and receive data
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(addr)
+        s.sendall(payload.encode())
+        s.shutdown(socket.SHUT_WR)
+
+        full_data = s.recv(BUFFER_SIZE)
+        print(full_data)
+
+    except Exception as e:
+        print(e)
+    finally:
+        s.close()
+
+def main():
+    connect((HOST,PORT))
+
+if __name__ == "__main__":
+    main()
